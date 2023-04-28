47eb9d48c655a03401c8aacebc9650e5330b805d
b4rtuus <bartixfajny@gmail.com>
2023-04-28 17:03:18 +0200

    bug fix: anime  slider

--- client/src/components/slider/index.jsx
+++ client/src/components/slider/index.jsx
@@ -1,27 +1,32 @@
-import { Swiper, SwiperSlide } from "swiper/react"
-import { Autoplay } from "swiper"
-import AnimeCard from "../animeCard"
-import { Flex, Text } from "@chakra-ui/react"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+import AnimeCard from "../animeCard";
+import { Flex, Text } from "@chakra-ui/react";
 
-export default ({
-    label, desc, items
-}) => {
-    return <>
-        <Flex mt={10} flexDir={'column'} pl={3} >
-            <Text fontSize={20} >{label}</Text>
-            <Text mb={3}>{desc}</Text>
-        </Flex>
-        <Swiper
+export default ({ label, desc, items }) => {
+  return (
+    <>
+      <Flex mt={10} flexDir={"column"} pl={3}>
+        <Text fontSize={20}>{label}</Text>
+        <Text mb={3}>{desc}</Text>
+      </Flex>
+      <Swiper
         slidesPerView={"auto"}
-        pagination={{clickable: true}}
-        autoplay={{delay: 3000, disableOnInteraction: false}}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
         modules={[Autoplay]}
-        >
-            {items.map(item =>
-                <SwiperSlide className="yuugen-slide">
-                    <AnimeCard title={item?.title} image={item?.img} slug={item?.slug} key={item?.slug} />
-                </SwiperSlide>
-                )}
-        </Swiper>
+      >
+        {items.map((item) => (
+          <SwiperSlide className="yuugen-slide">
+            <AnimeCard
+              title={item?.title}
+              image={item?.img}
+              slug={item?.slug}
+              key={item?.slug}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
-}
+  );
+};
